3c6bc17f12f802b45762e216e84939a198eb9438
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-11-08 14:32:59 -0300

    ✨ feat: Add and Configure Next/Auth for Google Login with mongodb Database

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,46 @@
 import { GetServerSideProps, InferGetStaticPropsType } from 'next';
+import { getSession, useSession } from 'next-auth/react';
+import Image from 'next/image';
 import { useEffect } from 'react';
-
-import { api } from '../lib/axios';
 
 export default function Home(
 	props: InferGetStaticPropsType<typeof getServerSideProps>
 ) {
+	const { data: session } = useSession();
+
 	useEffect(() => {
 		console.log(props);
 	}, []);
 
 	return (
-		<div className='text-3xl font-bold underline'>
-			<h1>hello World.</h1>
+		<div className='text-3xl font-bold underline text-white flex flex-col justify-center items-center'>
+			<h1>hello World: {session.user.name}</h1>
+			<Image
+				src={session.user.image}
+				alt='icon'
+				width={96}
+				height={96}
+				layout='fixed'
+			/>
 		</div>
 	);
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-	const { data } = await api.get('/api/hello');
+	const session = await getSession(context);
+
+	if (!session) {
+		return {
+			redirect: {
+				destination: '/login',
+				permanent: false,
+			},
+		};
+	}
 
 	return {
 		props: {
-			data: data,
+			session,
 		},
 	};
 };
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,10 @@
+import { GetServerSideProps } from 'next';
+import { getSession, signIn } from 'next-auth/react';
+import Image from 'next/image';
 import React from 'react';
+import { FcGoogle } from 'react-icons/fc';
+
+import logo from '../../assets/logowhite.png';
 
 export default function Home() {
 	return (
@@ -15,8 +21,42 @@ export default function Home() {
 			</div>
 
 			<div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
-				<div className='p-5'></div>
+				<div className='p-5'>
+					<Image src={logo} className='w-[130px]' alt='logo' />
+				</div>
+
+				<div className='shadow-2xl'>
+					<button
+						onClick={() =>
+							signIn('google', {
+								callbackUrl: 'http://localhost:3000',
+							})
+						}
+						type='button'
+						className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'>
+						<FcGoogle className='mr-4' /> Sign in With Google
+					</button>
+				</div>
 			</div>
 		</div>
 	);
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const session = await getSession(context);
+
+	if (session) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			},
+		};
+	}
+
+	return {
+		props: {
+			session,
+		},
+	};
+};
