a85c97aa5835906273e6882365539f24463d6d1f
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-12-11 15:27:58 -0300

    ✨ feat: Add Search view

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+import { Feed } from '../components';
+import { usePost } from '../context/usePost';
+
 export const Search = () => {
 	const { content } = useParams();
-	console.log(content);
-	return <div>Search</div>;
+	const { Posts } = usePost();
+
+	const filteredPosts = Posts.filter((post) =>
+		post.title.toLowerCase().includes(content.toLowerCase())
+	);
+	return (
+		<div className='flex flex-col gap-2'>
+			<h2 className='font-bold'>
+				Search: <span className='font-normal'>{content}</span>
+			</h2>
+			<Feed posts={filteredPosts} />;
+		</div>
+	);
 };
